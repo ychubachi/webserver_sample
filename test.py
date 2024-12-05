ff91fbe928857c8f14a71b1fc8875260f3f93e9d
Yoshihide Chubachi <yc@aiit.ac.jp>
2024-12-05 16:26:51 +0900

    streamlit

--- test.py
+++ test.py
@@ -1,25 +1,6 @@
-import http.server
-import socketserver
-import datetime
+import streamlit as st
 
-class CustomHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        self.wfile.write('GETメソッドを実装'.encode())
+input_num = st.number_input('Input a number', value=0)
 
-        # 現在の日付と時刻を取得
-        now = datetime.datetime.now()
-        # 結果を表示
-        print("現在の時刻:", now)
-        self.wfile.write(now.strftime("%Y-%m-%d %H:%M:%S").encode())
-
-PORT = 8000
-# Handler = http.server.SimpleHTTPRequestHandler
-Handler = CustomHTTPRequestHandler
-
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port ", PORT)
-    httpd.serve_forever()
-
+result = input_num ** 2
+st.write('Result: ', result)
