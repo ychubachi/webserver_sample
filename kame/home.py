d19f0e3b486a4b3d32c604171a07060b21cdfae8
Takauka <k22m2022@cis.twcu.ac.jp>
2024-12-19 16:10:43 +0900

    Update home.py

--- kame/home.py
+++ kame/home.py
@@ -12,17 +12,32 @@ with st.sidebar:
     st.header("現在の選択")
     st.write(f"サイコロの種類: **{choice}**")
 
+#ファイルパス
+path1='pages/saikoro_6.py'
+path2='pages/saikoro_12.py'
+path3='pages/saikoro_17.py'
+
 # 各オプションに応じたロジック
 if choice == "６種類":
     # ページのロジックを関数化して直接呼び出す
     st.subheader("６種類のサイコロ")
     st.write("ここでは６種類のサイコロを使います！")
+    f1 = open(path1)
+    content1=f1.read()
+    exec(content1)
     # ここに６種類のロジックを実装する
 elif choice == "12種類":
     st.subheader("12種類のサイコロ")
     st.write("ここでは12種類のサイコロを使います!")
+    f2 = open(path2)
+    content2=f2.read()
+    exec(content2)
     # ここに12種類のロジックを実装する
 elif choice == "17種類":
     st.subheader("17種類のサイコロ")
     st.write("ここでは17種類のサイコロを使います!")
+    f3 = open(path3)
+    content3=f3.read()
+    exec(content3)
     # ここに17種類のロジックを実装する
+
