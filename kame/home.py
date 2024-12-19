aafd18019d4b320ba08afd134d6f818d1ec36ce3
Takauka <k22m2022@cis.twcu.ac.jp>
2024-12-19 16:54:05 +0900

    画面を整理しました。

--- kame/home.py
+++ kame/home.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("絶対に大吉が出るアプリ")
+st.title("おみくじアプリ")
 ##st.write("以下からオプションを選択してください。")
 
 # オプションを選択するセレクトボックス
@@ -20,22 +20,22 @@ path3='./pages/saikoro_17.py'
 # 各オプションに応じたロジック
 if choice == "６種類":
     # ページのロジックを関数化して直接呼び出す
-    st.subheader("６種類のサイコロ")
-    st.write("ここでは６種類のサイコロを使います！")
+    #st.subheader("６種類のサイコロ")
+    #st.write("ここでは６種類のサイコロを使います！")
     f1 = open(path1)
     content1=f1.read()
     exec(content1)
     # ここに６種類のロジックを実装する
 elif choice == "12種類":
-    st.subheader("12種類のサイコロ")
-    st.write("ここでは12種類のサイコロを使います!")
+    #st.subheader("12種類のサイコロ")
+    #st.write("ここでは12種類のサイコロを使います!")
     f2 = open(path2)
     content2=f2.read()
     exec(content2)
     # ここに12種類のロジックを実装する
 elif choice == "17種類":
-    st.subheader("17種類のサイコロ")
-    st.write("ここでは17種類のサイコロを使います!")
+    #st.subheader("17種類のサイコロ")
+    #st.write("ここでは17種類のサイコロを使います!")
     f3 = open(path3)
     content3=f3.read()
     exec(content3)
