a26c9b2147b536f8988cccea4799f8f93c419af6
ShioriFUNAKI <k22m2039@cis.twcu.ac.jp>
2024-12-19 16:06:47 +0900

    6種類のおみくじの部分を一部修正しました。2

--- kame/pages/saikoro_6.py
+++ kame/pages/saikoro_6.py
@@ -5,15 +5,15 @@ st.title("おみくじ")
 
 if st.button("おみくじをひく"):
     result = random.randint(1, 6)
-    if result % 37 == 0:
+    if result == 1:
        st.write(f"{result}番 大吉です。")
-    elif result % 31== 0:
+    elif result == 2:
        st.write(f"{result}番 中吉です。")
-    elif result % 29 == 0:
+    elif result == 3:
        st.write(f"{result}番 小吉です。")
-    elif result % 23 == 0:
+    elif result == 4:
        st.write(f"{result}番 末吉です。")
-    elif result % 19 ==0:
+    elif result == 5:
        st.write(f"{result}番  凶です。")
     else:
        st.write(f"{result}番 大凶 です。")
