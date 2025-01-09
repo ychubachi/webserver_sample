b0fee593d124d57dd3a62f7f4dd873ba3e9a17aa
IshiwataAya <k22m2005@cis.twcu.ac.jp>
2025-01-09 15:17:31 +0900

    page1を編集しました

--- usagi/pages/page1.py
+++ usagi/pages/page1.py
@@ -1,42 +1,161 @@
 import streamlit as st
 import datetime
+from PIL import Image
 
-with st.form(key='profile_form'): #送信されないとリロードされない
-    #テキストボックス
-        name = st.text_input('名前')
-        #print(name) カーソルずれたら名前ターミナルに表示される
-        address = st.text_input('住所')
 
-        #セレクトボックス
-        age_category = st.radio(  #selectboxなら上からピロピロ radioは〇で選択
-            '年齢層',
-            ('子供(18才未満)','大人(18才以上)'))
+st.title('ウサギの友人相性占い')
+
+col1, col2=st.columns(2)
+with col1:
+    image_1 = Image.open('./data/img_4.png')
+    st.image(image_1, width=100)
+
+with col2:
+     image_2 = Image.open('./data/img_5.png')
+     st.image(image_2, width=100)
+
+# 占いロジック
+def get_fortune(answers):
+    m_s_1, m_s_2, animal_1, animal_2, food_1, food_2= answers
+    #3つ同じ
+    if  m_s_1=="山" and m_s_2=="山" and animal_1=="犬" and animal_2=="犬" and food_1=="和食" and food_2=='和食':
+        return "相性90%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="犬" and animal_2=="犬" and food_1=="洋食" and food_2=='洋食':
+        return "相性90%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="猫" and animal_2=="猫" and food_1=="和食" and food_2=='和食':
+        return "相性90%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="猫" and animal_2=="猫" and food_1=="洋食" and food_2=='洋食':
+        return "相性90%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="犬" and animal_2=="犬" and food_1=="和食" and food_2=='和食':
+        return "相性90%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="犬" and animal_2=="犬" and food_1=="洋食" and food_2=='洋食':
+        return "相性90%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="猫" and animal_2=="猫" and food_1=="和食" and food_2=='和食':
+        return "相性90%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="猫" and animal_2=="猫" and food_1=="洋食" and food_2=='洋食':
+        return "相性90%"
+    #2つ同じ（犬猫以外）
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="犬" and animal_2=="猫" and food_1=="和食" and food_2=='和食':
+        return "相性85%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="猫" and animal_2=="犬" and food_1=="和食" and food_2=='和食':
+        return "相性85%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="犬" and animal_2=="猫" and food_1=="洋食" and food_2=='洋食':
+        return "相性85%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="猫" and animal_2=="犬" and food_1=="洋食" and food_2=='洋食':
+        return "相性85%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="犬" and animal_2=="猫" and food_1=="和食" and food_2=='和食':
+        return "相性85%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="猫" and animal_2=="山" and food_1=="和食" and food_2=='和食':
+        return "相性85%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="犬" and animal_2=="猫" and food_1=="洋食" and food_2=='洋食':
+        return "相性85%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="猫" and animal_2=="犬" and food_1=="洋食" and food_2=='洋食':
+        return "相性85%"
+    #2つ同じ（山海以外）
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="犬" and animal_2=="犬" and food_1=="和食" and food_2=='和食':
+        return "相性83%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="犬" and animal_2=="犬" and food_1=="和食" and food_2=='和食':
+        return "相性83%"
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="犬" and animal_2=="犬" and food_1=="洋食" and food_2=='洋食':
+        return "相性83%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="犬" and animal_2=="犬" and food_1=="洋食" and food_2=='洋食':
+        return "相性83%"
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="猫" and animal_2=="猫" and food_1=="和食" and food_2=='和食':
+        return "相性83%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="猫" and animal_2=="猫" and food_1=="和食" and food_2=='和食':
+        return "相性83%"
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="猫" and animal_2=="猫" and food_1=="洋食" and food_2=='洋食':
+        return "相性83%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="猫" and animal_2=="猫" and food_1=="洋食" and food_2=='洋食':
+        return "相性83%"
+    #2つ同じ（和洋以外）
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="犬" and animal_2=="犬" and food_1=="和食" and food_2=='洋食':
+        return "相性80%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="犬" and animal_2=="犬" and food_1=="洋食" and food_2=='和食':
+        return "相性80%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="猫" and animal_2=="猫" and food_1=="和食" and food_2=='洋食':
+        return "相性80%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="猫" and animal_2=="猫" and food_1=="洋食" and food_2=='和食':
+        return "相性80%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="犬" and animal_2=="犬" and food_1=="和食" and food_2=='洋食':
+        return "相性80%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="犬" and animal_2=="犬" and food_1=="洋食" and food_2=='和食':
+        return "相性80%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="猫" and animal_2=="猫" and food_1=="和食" and food_2=='洋食':
+        return "相性80%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="猫" and animal_2=="猫" and food_1=="洋食" and food_2=='和食':
+        return "相性80%"
+    #1つ同じ（和洋同じ）
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="犬" and animal_2=="猫" and food_1=="和食" and food_2=='和食':
+        return "相性75%"
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="猫" and animal_2=="犬" and food_1=="和食" and food_2=='和食':
+        return "相性75%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="犬" and animal_2=="猫" and food_1=="和食" and food_2=='和食':
+        return "相性75%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="猫" and animal_2=="犬" and food_1=="和食" and food_2=='和食':
+        return "相性75%"
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="犬" and animal_2=="猫" and food_1=="洋食" and food_2=='洋食':
+        return "相性75%"
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="猫" and animal_2=="犬" and food_1=="洋食" and food_2=='洋食':
+        return "相性75%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="犬" and animal_2=="猫" and food_1=="洋食" and food_2=='洋食':
+        return "相性75%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="猫" and animal_2=="犬" and food_1=="洋食" and food_2=='洋食':
+        return "相性75%"
+    #1つ同じ（山海同じ）
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="犬" and animal_2=="猫" and food_1=="和食" and food_2=='洋食':
+        return "相性71%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="犬" and animal_2=="猫" and food_1=="洋食" and food_2=='和食':
+        return "相性71%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="猫" and animal_2=="犬" and food_1=="和食" and food_2=='洋食':
+        return "相性71%"
+    elif  m_s_1=="山" and m_s_2=="山" and animal_1=="猫" and animal_2=="犬" and food_1=="洋食" and food_2=='和食':
+        return "相性71%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="犬" and animal_2=="猫" and food_1=="和食" and food_2=='洋食':
+        return "相性71%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="犬" and animal_2=="猫" and food_1=="洋食" and food_2=='和食':
+        return "相性71%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="猫" and animal_2=="犬" and food_1=="和食" and food_2=='洋食':
+        return "相性71%"
+    elif  m_s_1=="海" and m_s_2=="海" and animal_1=="猫" and animal_2=="犬" and food_1=="洋食" and food_2=='和食':
+        return "相性71%"
+    #1つ同じ（犬猫同じ）
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="犬" and animal_2=="犬" and food_1=="和食" and food_2=='洋食':
+        return "相性68%"
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="犬" and animal_2=="犬" and food_1=="洋食" and food_2=='和食':
+        return "相性68%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="犬" and animal_2=="犬" and food_1=="和食" and food_2=='洋食':
+        return "相性68%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="犬" and animal_2=="犬" and food_1=="洋食" and food_2=='和食':
+        return "相性68%"
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="猫" and animal_2=="猫" and food_1=="和食" and food_2=='洋食':
+        return "相性68%"
+    elif  m_s_1=="山" and m_s_2=="海" and animal_1=="猫" and animal_2=="猫" and food_1=="洋食" and food_2=='和食':
+        return "相性68%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="猫" and animal_2=="猫" and food_1=="和食" and food_2=='洋食':
+        return "相性68%"
+    elif  m_s_1=="海" and m_s_2=="山" and animal_1=="猫" and animal_2=="猫" and food_1=="洋食" and food_2=='和食':
+        return "相性68%"
+    else:
+        return "相性50%"
+
+with st.form("fortune_form"):
+            q1 = st.radio("あなたは山と海どちらに行くのが好きですか？", ["山", "海"], index=0)
+            q2 = st.radio("相手は山と海どちらに行くのが好きですか？", ["山", "海"], index=0)
+            q3 = st.radio("あなたは犬と猫のどちらが好きですか？", ["犬", "猫"], index=0)
+            q4 = st.radio("相手は犬と猫のどちらが好きですか？", ["犬", "猫"], index=0)
+            q5 = st.radio("あなたは和食と洋食のどちらが好きですか？", ["和食", "洋食"], index=0)
+            q6 = st.radio("相手は和食と洋食どちらが好きですか？", ["和食", "洋食"], index=0)
         
-        #複数選択
-        hobby = st.multiselect(
-            '趣味',
-            ('スポーツ','読書','プログラミング','アニメ・映画','釣り','料理'))
+            
+            submit_button = st.form_submit_button("占う")
+            cancel_btn = st.form_submit_button('キャンセル')
 
-        #チェックボックス
-        mail_subscribe = st.checkbox('メールマガジンを購読する')
+            if submit_button:
+                answers = [q1, q2, q3,q4,q5,q6]
+                result = get_fortune(answers)
+                st.success(f"結果: {result}")
 
-        #スライダー
-        height = st.slider('身長',min_value=110, max_value=210)
 
-        #日付
-        start_date=st.date_input(
-            '開始日',
-            datetime.date(2024,12,1))
-        
-        #カラーピッカー
-        color = st.color_picker('テーマカラー','#00f900')
+mail_subscribe=st.checkbox('占い結果をメールアドレスに送る')
 
-        #ボタン
-        submit_btn = st.form_submit_button('送信')
-        cancel_btn = st.form_submit_button('キャンセル')
-    #print(f'submit_btn: {submit_btn}') 押されるとターミナルにTrueと表示
-    #print(f'cancel_btn: {cancel_btn}')
-        if submit_btn:
-            st.text(f'ようこそ！{name}さん！{address}に書籍を送りました') #Web画面に表示
-            st.text(f'年齢層：{age_category}')
-            st.text(f'趣味： {", ".join(hobby)}') #カンマ区切り
