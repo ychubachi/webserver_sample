0a46613497edbb2dcc0ddbf84a298cef42336655
ShioriFUNAKI <k22m2039@cis.twcu.ac.jp>
2025-01-16 15:34:37 +0900

    プログラムを一部修正しました。

--- kame/omikuji_app.py
+++ kame/omikuji_app.py
@@ -1,10 +1,14 @@
 import random
 import streamlit as st
 
+if "omikuji" not in st.session_state:
+   st.session_states.omikuji = []
+
 st.title("おみくじ")
 
 if st.button("おみくじをひく"):
     result = random.randint(1, 6)
+    st.session_state.omikuji.append((result))
     if result == 1:
        st.write(f"あなたの運勢は「大吉」です")
     elif result == 2:
@@ -17,3 +21,7 @@ if st.button("おみくじをひく"):
        st.write(f"あなたの運勢は「凶」です")
     else:
        st.write(f"あなたの運勢は「大凶」です")
+
+df = pd.DataFrame(st.session_state.omikuji, colums=["おみくじ番号"])
+st.dateframe(df)
+st.write("おみくじをひいた回数", len(st.session_state.omikuji))
