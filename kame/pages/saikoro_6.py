daa3ea35fac23ff7c12cdadcb52f736751ad9ddb
ShioriFUNAKI <k22m2039@cis.twcu.ac.jp>
2025-03-04 15:02:09 +0900

    おみくじの結果表示方法を一部変更しました。

--- kame/pages/saikoro_6.py
+++ kame/pages/saikoro_6.py
@@ -9,28 +9,39 @@ if "omikuji" not in st.session_state:
 st.title("おみくじ")
 
 if st.button("おみくじをひく"):
-    result = random.randint(1, 6)
-    st.session_state.omikuji.append((result))
-    if result == 1:
-       st.write(f"{result}番 大吉です。")
-    elif result == 2:
-       st.write(f"{result}番 中吉です。")
-    elif result == 3:
-       st.write(f"{result}番 小吉です。")
-    elif result == 4:
-       st.write(f"{result}番 吉です。")
-    elif result == 5:
-       st.write(f"{result}番 末吉です。")
+    result = random.randint(1, 120)
+    if result%6 == 1:
+       result2 = "大吉"
+       st.write(f"{result}番 {result2}です。")
+       st.session_state.omikuji.append((result, result2))
+    elif result%6 == 2:
+       result2 = "中吉"
+       st.write(f"{result}番 {result2}です。")
+       st.session_state.omikuji.append((result, result2))
+    elif result%6 == 3:
+       result2 = "小吉"
+       st.write(f"{result}番 {result2}です。")
+       st.session_state.omikuji.append((result, result2))
+    elif result%6 == 4:
+       result2 = "吉"
+       st.write(f"{result}番 {result2}です。")
+       st.session_state.omikuji.append((result, result2))
+    elif result%6 == 5:
+       result2 = "末吉"
+       st.write(f"{result}番 {result2}です。")
+       st.session_state.omikuji.append((result, result2))
     else:
-       st.write(f"{result}番 凶 です。")
+       result2 = "凶"
+       st.write(f"{result}番 {result2}です。")
+       st.session_state.omikuji.append((result, result2))
 
 #画像
 image = Image.open('kame/6omikuji.png')
 st.image(image, width=700)
 
-df = pd.DataFrame(st.session_state.omikuji, columns=["おみくじ番号"])
+df = pd.DataFrame(st.session_state.omikuji, columns=["おみくじ番号", "運勢"])
 st.dataframe(df)
 st.write("おみくじをひいた回数", len(st.session_state.omikuji))
 
 if st.button("結果"):
-   st.bar_chart(df["おみくじ番号"].value_counts())
+   st.bar_chart(df["運勢"].value_counts())
